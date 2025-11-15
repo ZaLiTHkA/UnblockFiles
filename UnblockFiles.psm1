dc21224acf104380c0483e56c874f953ef856ea5
Andre Greeff <little.grief@gmail.com>
2025-11-15 21:49:16 +0200

    feat: improve user feedback during runtime

--- UnblockFiles.psm1
+++ UnblockFiles.psm1
@@ -4,27 +4,29 @@ function Unblock-Files {
     [int]$Depth = [int]::0
   )
 
+  Write-Host "Launching Unblock-Files Helper" -ForegroundColor Green
+
   $depthLabel = "$Depth levels"
   if ($Depth -eq -1) {
     $Depth = [int]::MaxValue
     $depthLabel = "infinitely"
   }
-  Write-Host "looking for files, going $depthLabel deep from '$Path'..."
+  Write-Host "looking for files, going $depthLabel deep from '$Path'..." -ForegroundColor Gray
 
   $files = Get-ChildItem -Path $Path -File -Recurse -Depth $Depth
   if (-not $files) {
-    Write-Host "no files found to process, please check your command and try again..."
+    Write-Host "no files found to process, please check your command and try again..." -ForegroundColor Red
     return
   }
 
-  Write-Host "located $($files.Count) files worth checking..."
+  Write-Host "located $($files.Count) files worth checking..." -ForegroundColor Gray
   if (-not (Read-Host "would you like to continue? (y/N)").ToLower().StartsWith('y')) {
-    Write-Host "exiting script, no changes will be made."
+    Write-Host "exiting script, no changes were made." -ForegroundColor Red
     return
   }
 
   $files | Unblock-File
-  Write-Host "successfully executed 'Unblock-File' on $($files.Count) files."
+  Write-Host "successfully executed 'Unblock-File' on $($files.Count) files." -ForegroundColor Green
 }
 
 Export-ModuleMember -Function 'Unblock-Files'
